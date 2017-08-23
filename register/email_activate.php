1b0c557ab15e80f8818266a784e85c11f9282885
svkrdj <30564616+svkrdj@users.noreply.github.com>
2017-08-23 16:13:01 +0530

    Update email_activate.php

--- register/email_activate.php
+++ register/email_activate.php
@@ -1,12 +1,12 @@
 <?php
-include '/storage/ssd2/679/2427679/public_html/init.php';
-include '/storage/ssd2/679/2427679/public_html/user.php';
+include 'init.php';
+include 'user.php';
 if(isset($_GET['usrn']) && isset($_GET['email_code']))
 {
    $username=$_GET['usrn'];
    $email_code=$_GET['email_code'];
-   $conn = new mysqli('localhost', 'id2427679_svkrclg', 'happyacct', 'id2427679_user_list');
-   $sql = "UPDATE user 
+   $conn = new mysqli('your_host', 'username', 'password', 'databasename');
+   $sql = "UPDATE tablename 
           SET email_state='1'
           WHERE `username`= '$username' AND `email_code`='$email_code'";
 	   if ($conn->query($sql) === true) 
